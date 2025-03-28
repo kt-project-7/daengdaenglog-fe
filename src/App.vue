769a4fde34b7f300efa69f22bda2ec2d2bb4ba31
yoonha <yoonha0124@naver.com>
2025-03-28 09:41:47 +0900

    chore: 폰트 추가 및 정민 작업 초기화

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="p-4 text-center">
     <img src="@/assets/logo.svg" alt="Logo" class="w-32 h-32 mx-auto" />
-    <h1 class="text-2xl mt-4 font-bold">DaengDaengLog</h1>
+    <h1 class="title-1 mt-4">DaengDaengLog</h1>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['Pretendard', 'sans-serif'],
+        body: ['Pretendard', 'sans-serif'],
+      },
+      colors: {
+        primary: '#0046FF',
+        _black: '#313131',
+        _red: '#EA4141',
+        '_gray-100': '#F4F4F4',
+        '_gray-200': '#D2D2D2',
+        '_gray-300': '#929292',
+        '_gray-400': '#626262',
+      },
+    },
   },
   plugins: [],
 }
