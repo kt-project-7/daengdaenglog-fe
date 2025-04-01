bc7510ca6eb0e944b1bb3f8ea42f07d651666245
yoonha <yoonha0124@naver.com>
2025-04-01 09:58:44 +0900

    revert: 커밋 되돌리기

--- src/apis/auth.js
+++
@@ -1,7 +0,0 @@
-import api from './axios';
-export const signIn = async (data) => {
-    const response = await api.post('/auth/sign-in', data);
-    const accessToken = response.headers.authorization;
-    const user = response.data.results;
-    return { accessToken, user };
-};
